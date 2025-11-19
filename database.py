ab10ff42ebdd14d957e81476e9e0db1494040e35
genki <g.lopessatanacruz@gmail.com>
2025-11-19 14:07:58 -0300

    push

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("https://yziiavkxztzssvhxhotb.supabase.co")
-SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6aWlhdmt4enR6c3N2aHhob3RiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzU2MzkxOCwiZXhwIjoyMDc5MTM5OTE4fQ.SWkMxdY5V6R_ECiGHrkVK92SDQKere-WLX4XjzI-QpI")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
